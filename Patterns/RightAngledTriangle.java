b166c04f80fb878909692214959face907a5f4ca
llavi-16 <pallavisingh16th@gmail.com>
2025-07-08 23:29:25 +0530

    Add RightAngledTriangle class for printing a right-angled triangle pattern

---
+++ Patterns/RightAngledTriangle.java
@@ -0,0 +1,14 @@
+public class RightAngledTriangle{
+    public static void main(String[] args){
+        pattern(4);
+
+    }
+    public static void pattern(int n){
+        for(int i =1; i<=n;i++){
+            for(int j =1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
