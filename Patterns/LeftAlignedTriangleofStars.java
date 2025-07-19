9a497eb45c492c32da218078f843a21b0630c14b
llavi-16 <pallavisingh16th@gmail.com>
2025-07-19 23:31:42 +0530

    Fix syntax errors in LeftAlignedTriangleofStars class

--- Patterns/LeftAlignedTriangleofStars.java
+++ Patterns/LeftAlignedTriangleofStars.java
@@ -1,6 +1,13 @@
 public class LeftAlignedTriangleofStars{
-    public class static void main(String[] args){
+    public static void main(String[] args){
         pattern (5);
     }
-    public static void ma
+    public static void pattern(int n){
+        for(int i =1;i<=n;i++){
+            for(int j =1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
