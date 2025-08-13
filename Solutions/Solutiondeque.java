6b7494249c54dd5d6a247872779178ba2cdcfed8
llavi-16 <pallavisingh16th@gmail.com>
2025-08-13 12:23:06 +0530

    Add multiple solution classes for various algorithms including prime factorization, target indices, maximum count, peak element, and area calculation based on user choice

--- Solutions/Solutiondeque.java
+++ Solutions/Solutiondeque.java
@@ -1,16 +1,6 @@
-class Solution2529 {
-    public int maximumCount(int[] nums) {
-        int pos = 0, neg = 0;
-        for (int num : nums) {
-            if (num > 0) pos++;
-            else if (num < 0) neg++;
-        }
-        return Math.max(pos, neg);
-    }
-}
 import java.util.*;
 
-class Solution {
+class Solutiondeque {
     public static void pb(ArrayDeque<Integer> dq, int x) {
         // Push to back
         dq.addLast(x);
